7c980d70aabdd86a9d8ad68c3090f10541b51827
vwillyams <v.willyams@gmail.com>
2015-07-04 19:17:47 +1200

    Switched over to anchor-based references, this is much better but breaks Angular inclusion (this is to be expected though really)

--- README.md
+++ README.md
@@ -7,15 +7,15 @@ need to reference later on in your page like so:
 
 \[link name\]:http://vwillyams.github.com/ "My Github page!"
 
-The end result will look an awful lot like this[<sup>1</sup>][vwillyams]
+Then, you will need to add a place to display all your references, using the <references> tag.
+
+The end result will look an awful lot like this[<sup>1</sup>](#1)
+
+<a name="1">1</a>: VWillyams Github
 
 [vwillyams]:http://vwillyams.github.com/ "My Github page!"
 
 These links are indexed in order of appearance on the page, so you don't need to remember that you're citing item number 47 - just remember whatever shorthand name you chose to give your citation.
-
-## Known Issues
-
-- Currently I don't have an easy method of printing out the reference sheet. This is my top priority right now.
 
 ## Client-side
 
--- showdown-footnotify-extension.js
+++ showdown-footnotify-extension.js
@@ -7,24 +7,38 @@
 (function() {
 
   var footnotify = function () {
+    // TODO should probably actually BE an array, I don't really need to store indexes or key off footnote names just for simpler lookup code
     var footnoteArray = {};
     var idx = 1;
 
     return [
       {
         type:    'lang',
-        regex:   '\\[(\\^)\\]\\(([^\\r\\n\\)]*)\\)',
+        regex:   '\\[(\\^)\\]\\(([^\\r\\n\\#\\/\\"\\)]*)\\)',
         replace: function (match, link, linkText) {
           var thisIdx;
-          var indexingLink = linkText.toLowerCase();
-          if(!footnoteArray[indexingLink]){
-            footnoteArray[indexingLink] = idx;
+          if(!footnoteArray[linkText]){
+            footnoteArray[linkText] = idx;
             thisIdx = idx;
             idx += 1;
           } else {
-            thisIdx = footnoteArray[indexingLink];
+            thisIdx = footnoteArray[linkText];
           }
-          return '[<sup>' + thisIdx + '</sup>][' + linkText + ']';
+          return '[<sup>' + thisIdx + '</sup>](#' + thisIdx + ')';
+        }
+      },
+      {
+        type:    'lang',
+        regex:   '<references>',
+        replace: function (match) {
+          var rtn = [];
+          // TODO and here's where the obvious lazy array problem starts to bite...
+          for(var i = 0; i < Object.keys(footnoteArray).length; i++){
+            var linkText = Object.keys(footnoteArray)[i];
+            rtn += "<a name="+ footnoteArray[linkText] + ">" + footnoteArray[linkText] + "</a>: [" + linkText + "][" + linkText + "] \n\n";
+          }
+          console.log(rtn);
+          return rtn;
         }
       }
     ];
